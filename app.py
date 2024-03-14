662993375edbd5594ab5b1c094b4ae5b5e9cf3c1
Pran luis <pranluisgaming143@gmail.com>
2024-03-15 00:43:32 +0530

    Deploy server program

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 # Importing important modules in the app
-from flask import redirect, render_template, request, json
+from flask import Flask, redirect, render_template, request, json
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return 'hello world'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
