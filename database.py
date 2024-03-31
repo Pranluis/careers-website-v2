ba4b9f438708dbd82ccd6b053449a6ff8f41a602
Pran luis <pranluisgaming143@gmail.com>
2024-03-31 19:56:54 +0530

    added new data in database

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import sqlite3
 def add_sql_commands():
     conn = sqlite3.connect("luidlinkcareer.db")
     cursor = conn.cursor()
-    enter_data = f'INSERT INTO jobs VALUES (7,"Full Stack Developer", "Mumbai, India", 1500000, "rupees", "You have to set backend system and design website with database", "MERN")'
+    enter_data = f'INSERT INTO jobs VALUES (8,"Game Developer", "Mumbai, India", 7000, "dollars", "You have to create games on unity", "Unity, C++")'
     cursor.execute(enter_data)  
     conn.commit()
     conn.close()
