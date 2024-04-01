dfee0db29ca69df5119f2265e63931b6a303fdd8
Pran luis <pranluisgaming143@gmail.com>
2024-04-02 01:07:50 +0530

    added new jobpage

--- app.py
+++ app.py
@@ -1,39 +1,27 @@
 # Importing important modules in the app
 from flask import Flask, redirect, render_template, request, json, jsonify
 import sqlite3
+from database import load_jobs, load_job_from_db
 app = Flask(__name__)
 
-def load_jobs():
-    conn = sqlite3.connect("luidlinkcareer.db")
-    cursor = conn.cursor()
-    get_data = f"SELECT * FROM jobs"
-    cursor.execute(get_data)
-    rows = cursor.fetchall()
-
-    def tuples_to_dict(keys, values):
-        return dict(zip(keys, values))
-    
-    keys = [description[0] for description in cursor.description]
-    jobs = [tuples_to_dict(keys, row) for row in rows]
-    conn.close()
-    return jobs
 
 @app.route("/")
 def home():
     JOBS = load_jobs()
-    conn = sqlite3.connect("luidlinkcareer.db")
-    cursor = conn.cursor()
-    car_entry = '''CREATE TABLE IF NOT EXISTS jobs
-            (id INT NOT NULL PRIMARY KEY, title VARCHAR(250) NOT NULL, location VARCHAR(250) NOT NULL, salary INT, currency VARCHAR(250), responsibilities VARCHAR(2000), requirements VARCHAR(2000))'''
-    conn.execute(car_entry)
-    conn.commit()
-    conn.close()
     return render_template('home.html', jobs=JOBS)
 
 @app.route("/api/jobs")
 def list_jobs():
     JOBS = load_jobs()
     return jsonify(JOBS)
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    if not job:
+        return "Not found", 404
+    
+    return render_template('jobpage.html', job=job)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -8,5 +8,34 @@ def add_sql_commands():
     conn.commit()
     conn.close()
 
-if __name__ == '__main__':
-    add_sql_commands()
+def load_jobs():
+    conn = sqlite3.connect("luidlinkcareer.db")
+    cursor = conn.cursor()
+    get_data = f"SELECT * FROM jobs"
+    cursor.execute(get_data)
+    rows = cursor.fetchall()
+
+    def tuples_to_dict(keys, values):
+        return dict(zip(keys, values))
+    
+    keys = [description[0] for description in cursor.description]
+    jobs = [tuples_to_dict(keys, row) for row in rows]
+    conn.close()
+    return jobs
+
+def load_job_from_db(id):
+    conn = sqlite3.connect("luidlinkcareer.db")
+    cursor = conn.cursor()
+    get_data = f"SELECT * FROM jobs WHERE id = '{id}'"
+    cursor.execute(get_data)
+    vals = cursor.fetchall()
+    if len(vals) == 0:
+        return None
+    else:
+        def tuples_to_dict(keys, values):
+            return dict(zip(keys, values))
+        keys = [description[0] for description in cursor.description]
+        jobs = [tuples_to_dict(keys, val) for val in vals]
+        return jobs[0]
+    
+    
